5490ec64c3604670ae3bcbf2b475eb47638cffb9
James Hughes <james@virtualjames.com>
2012-09-21 13:56:48 -0700

    minor comment about how lazy I am

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -15,6 +15,8 @@ class Permission < ActiveRecord::Base
 
   attr_accessible(:kit_id, :exclusive_until, :expires_at)
 
+  # TODO: this is just lazy... what is this decorator doing in here?
+  # Figure out how to use decorated models with Simple Form.
   def data_text
     return if new_record?
     KitDecorator.decorate(kit).description
